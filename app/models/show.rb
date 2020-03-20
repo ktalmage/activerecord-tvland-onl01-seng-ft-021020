0bef9b4613eca64d15374641577fb1cc3dc8138a
Kyle Talmage <ktalmage85@gmail.com>
2020-03-20 13:50:21 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
   
+  def actors_list
+  
+  characters.each do |character|
+    character.actor
+  end
+   binding.pry
+  end
 end
